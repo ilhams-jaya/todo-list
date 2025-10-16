57d0c9b2b639fecc5ea4a72795cc1674b031bf48
ilhams-jaya <ilhamsjaya12@gmail.com>
2025-10-16 19:11:57 +0700

    update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 
-const db = new Sequelize('todo_list', 'root', '',{
+const db = new Sequelize('todo_list', 'root', '', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: mysql2
 });
 
 export default db;
