d7173ec0d6715ae6c29a1aeebcb2e9af372a5bc5
ilhams-jaya <ilhamsjaya12@gmail.com>
2025-10-16 20:10:28 +0700

    update

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,10 +1,10 @@
 import { Sequelize } from "sequelize";
 import mysql2 from "mysql2";
 
-const DB_NAME = process.env.DB_NAME || 'todo_list';
-const DB_USER = process.env.DB_USER || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || '';
-const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_NAME = process.env.DB_NAME || 'beteiunn4hvda1y23qpn';
+const DB_USER = process.env.DB_USER || 'u12rvxxmujfrqf3l';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'UduVjICpWhisQQvQRKkz';
+const DB_HOST = process.env.DB_HOST || 'beteiunn4hvda1y23qpn-mysql.services.clever-cloud.com';
 const DB_PORT = Number(process.env.DB_PORT || 3306);
 const DB_SSL = String(process.env.DB_SSL || '').toLowerCase() === 'true';
 
